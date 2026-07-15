37a0b781f7906f406541f063ac605852a9f5ad4a
carvalhomaykon <carvalhomaykon566@gmail.com>
2026-07-15 18:34:14 -0300

    atualiza a URL base da API para o endpoint correto

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'https://thomson-seq-thing-visited.trycloudflare.com';
+const API_BASE_URL = 'https://anthropology-recreation-poems-chips.trycloudflare.com';
+// const API_BASE_URL = 'http://127.0.0.1:8000';
 
 export const sendDiseaseData = async (data) => {
   const response = await fetch(`${API_BASE_URL}/registros`, {
